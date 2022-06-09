742206d9cf4f1003f5fbe41b9008427605e62cb8
arpitweetech <105038957+arpitweetech@users.noreply.github.com>
2022-06-09 15:39:07 +0530

    Update DisposePluggable.kt

--- android/src/main/kotlin/rekab/app/background_locator/pluggables/DisposePluggable.kt
+++ android/src/main/kotlin/rekab/app/background_locator/pluggables/DisposePluggable.kt
@@ -14,11 +14,13 @@ class DisposePluggable : Pluggable {
 
     override fun onServiceDispose(context: Context) {
         (PreferencesManager.getCallbackHandle(context, Keys.DISPOSE_CALLBACK_HANDLE_KEY))?.let { disposeCallback ->
-            val backgroundChannel = MethodChannel(IsolateHolderService.backgroundEngine?.dartExecutor?.binaryMessenger,
-                    Keys.BACKGROUND_CHANNEL_ID)
+            val backgroundChannel = IsolateHolderService.backgroundEngine?.dartExecutor?.binaryMessenger?.let {
+                MethodChannel(it,
+                        Keys.BACKGROUND_CHANNEL_ID)
+            }
             Handler(context.mainLooper)
                     .post {
-                        backgroundChannel.invokeMethod(Keys.BCM_DISPOSE,
+                        backgroundChannel?.invokeMethod(Keys.BCM_DISPOSE,
                                 hashMapOf(Keys.ARG_DISPOSE_CALLBACK to disposeCallback))
                     }
         }
